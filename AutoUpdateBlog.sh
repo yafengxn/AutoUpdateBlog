3aa81177ac1f0ff302b2eed180c91db0a0b4bc6b
yafengxn <feng.xiaoning@immomo.com>
2020-11-23 22:34:12 +0800

    fix 重复文章的问题

--- AutoUpdateBlog.sh
+++ AutoUpdateBlog.sh
@@ -8,9 +8,24 @@ PublishArticleName=${1}
 # 待发布文件路径
 PublishArticlePath=${PublishArticleSourcePath}/${PublishArticleName}
 
+# 校验是否已存在对应md文件
+verfiyArticleIfExist() {
+    for file in `ls "$PublishArticleEnvDir/_posts"`; do
+        result=$(echo $file | grep "${1}")
+        if [ "$result" != "" ]; then
+            echo "$file"
+            return
+        fi
+    done
+    echo ""
+}
+
 # 修改文件名为2020-11-04-misssemester-文件名，并拷贝到指定目录
 copyArticleToPublishEnv() {
-	PublishArticleName=$(date "+%Y-%m-%d")-"$PublishArticleDir"-$PublishArticleName
+    PublishArticleName=`verfiyArticleIfExist $1`
+    if [[ "$PublishArticleName" == "" ]]; then
+        PublishArticleName=$(date "+%Y-%m-%d")-"$PublishArticleDir"-$PublishArticleName
+    fi
 
 	if [ ! -f "$PublishArticlePath" ]; then
 		echo "❌Error: 待发布文章不存在, 请查看源文件是否存在"
@@ -20,13 +35,12 @@ copyArticleToPublishEnv() {
 	fi
 
 	# 拷贝
-    cp $PublishArticlePath                     $PublishArticleEnvDir/_posts/$PublishArticleName
+    cp $PublishArticlePath $PublishArticleEnvDir/_posts/$PublishArticleName
 	if [  $? -ne 0 ]; then
 		echo "❌Error: 待发布文章不存在, 请查看是否copy成功"
 		exit 0
 	fi
 }
-
 
 # 生成静态网页资源
 produceStaticHtmlResource() {
@@ -46,6 +60,7 @@ uploadFiles() {
 # main
 main() {
 	copyArticleToPublishEnv "$PublishArticleName"
+    produceStaticHtmlResource
 	uploadFiles "$PublishArticleName"
 }
 
